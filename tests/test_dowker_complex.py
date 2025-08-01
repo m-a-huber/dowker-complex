e7f902a6f8ced016e79788f8bf1663139faf40d6
m-a-huber <hubermarius@hotmail.com>
2025-08-01 11:13:56 +0200

    Update tests

--- tests/test_dowker_complex.py
+++ tests/test_dowker_complex.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split  # type: ignore
 
 from dowker_complex import DowkerComplex  # type: ignore
 
-rng = np.random.default_rng()
+rng = np.random.default_rng(42)
 
 
 @pytest.fixture
@@ -59,16 +59,16 @@ def octagon():
 
 def test_dowker_complex(random_data):
     """
-    Check whether `DowkerComplex` runs at all for all admissible choices of
-    `max_dimension` and produces complex of correct dimension.
+    Check whether `DowkerComplex` runs at all for `max_dimension` up to and
+    including `1`.
     """
     X, y = random_data
     for max_dimension in [0, 1]:
-        drc = DowkerComplex(max_dimension=max_dimension)
-        drc.fit_transform(X, y)
-        assert hasattr(drc, "persistence_")
-        assert hasattr(drc, "complex_")
-        assert drc.complex_.dimension() == max_dimension + 1
+        dc = DowkerComplex(max_dimension=max_dimension)
+        dc.fit_transform(X, y)
+        assert hasattr(dc, "persistence_")
+        assert hasattr(dc, "complex_")
+        assert dc.complex_.dimension() == max_dimension + 1
 
 
 def test_dowker_complex_cosine(random_data):
@@ -76,14 +76,15 @@ def test_dowker_complex_cosine(random_data):
     Check whether `DowkerComplex` runs on random data with non-default metric.
     """
     X, y = random_data
-    drc = DowkerComplex(metric="cosine")
-    drc.fit_transform(X, y)
-    assert hasattr(drc, "persistence_")
+    dc = DowkerComplex(metric="cosine")
+    dc.fit_transform(X, y)
+    assert hasattr(dc, "persistence_")
 
 
 def test_dowker_complex_empty_vertices():
     """
-    Check whether `DowkerComplex` runs for empty set of vertices.
+    Check whether `DowkerComplex` runs for empty set of vertices and yields
+    correct result.
     """
     X, y = (
         [
@@ -92,17 +93,17 @@ def test_dowker_complex_empty_vertices():
         ],
         None,
     )
-    drc = DowkerComplex()
-    drc.fit_transform(X, y)
-    assert hasattr(drc, "persistence_")
-    assert len(drc.persistence_) == 2
+    dc = DowkerComplex()
+    dc.fit_transform(X, y)
+    assert hasattr(dc, "persistence_")
+    assert len(dc.persistence_) == 2
     assert (
-        drc.persistence_[0] == np.empty(
+        dc.persistence_[0] == np.empty(
             (0, 2)
         )
     ).all()
     assert (
-        drc.persistence_[1] == np.empty(
+        dc.persistence_[1] == np.empty(
             (0, 2)
         )
     ).all()
@@ -119,46 +120,48 @@ def test_dowker_complex_empty_witnesses():
         ],
         None,
     )
-    drc = DowkerComplex()
-    drc.fit_transform(X, y)
-    assert hasattr(drc, "persistence_")
-    assert len(drc.persistence_) == 2
+    dc = DowkerComplex()
+    dc.fit_transform(X, y)
+    assert hasattr(dc, "persistence_")
+    assert len(dc.persistence_) == 2
     assert (
-        drc.persistence_[0] == np.empty(
+        dc.persistence_[0] == np.empty(
             (0, 2)
         )
     ).all()
     assert (
-        drc.persistence_[1] == np.empty(
+        dc.persistence_[1] == np.empty(
             (0, 2)
         )
     ).all()
 
 
-def test_dowker_complex_plotting_2d(random_data):
+def test_dowker_complex_empty_witnesses_no_swap():
     """
-    Check whether `DowkerComplex` plots 2D data.
+    Check whether `DowkerComplex` runs for empty set of witnesses with
+    `swap=False`.
     """
-    X, y = random_data
-    X = [pt_cloud[:, :2] for pt_cloud in X]
-    drc = DowkerComplex()
-    drc.fit_transform(X, y)
-    assert hasattr(drc, "persistence_")
-    drc.plot_points()
-    drc.plot_persistence()
-
-
-def test_dowker_complex_plotting_3d(random_data):
-    """
-    Check whether `DowkerComplex` plots 3D data.
-    """
-    X, y = random_data
-    X = [pt_cloud[:, :3] for pt_cloud in X]
-    drc = DowkerComplex()
-    drc.fit_transform(X, y)
-    assert hasattr(drc, "persistence_")
-    drc.plot_points()
-    drc.plot_persistence()
+    X, y = (
+        [
+            rng.standard_normal(size=(10, 512)),
+            rng.standard_normal(size=(0, 512)),
+        ],
+        None,
+    )
+    dc = DowkerComplex(swap=False)
+    dc.fit_transform(X, y)
+    assert hasattr(dc, "persistence_")
+    assert len(dc.persistence_) == 2
+    assert (
+        dc.persistence_[0] == np.empty(
+            (0, 2)
+        )
+    ).all()
+    assert (
+        dc.persistence_[1] == np.empty(
+            (0, 2)
+        )
+    ).all()
 
 
 def test_dowker_complex_quadrilateral(quadrilateral):
@@ -166,18 +169,20 @@ def test_dowker_complex_quadrilateral(quadrilateral):
     Check whether `DowkerComplex` returns correct result on small
     quadrilateral.
     """
-    drc = DowkerComplex()
-    drc.fit_transform(*quadrilateral)
-    assert hasattr(drc, "persistence_")
-    assert len(drc.persistence_) == 2
+    dc = DowkerComplex()
+    dc.fit_transform(*quadrilateral)
+    assert hasattr(dc, "persistence_")
+    assert len(dc.persistence_) == 2
     assert (
-        drc.persistence_[0] == np.array(
+        dc.persistence_[0] == np.array(
             [[1, np.inf]],
+            dtype=np.float64
         )
     ).all()
     assert (
-        drc.persistence_[1] == np.array(
+        dc.persistence_[1] == np.array(
             [[np.sqrt(5), 3]],
+            dtype=np.float64
         )
     ).all()
 
@@ -186,18 +191,18 @@ def test_dowker_complex_octagon(octagon):
     """
     Check whether `DowkerComplex` returns correct result on regular octagon.
     """
-    drc = DowkerComplex()
-    drc.fit_transform(*octagon)
-    assert hasattr(drc, "persistence_")
-    assert len(drc.persistence_) == 2
+    dc = DowkerComplex()
+    dc.fit_transform(*octagon)
+    assert hasattr(dc, "persistence_")
+    assert len(dc.persistence_) == 2
     birth = np.sqrt(2 - np.sqrt(2))
     death = np.sqrt(2 + np.sqrt(2))
     assert (
-        drc.persistence_[0] == np.array([
+        dc.persistence_[0] == np.array([
             [birth, death],
             [birth, np.inf]
-        ])
+        ], dtype=np.float64)
     ).all()
     assert (
-        drc.persistence_[1] == np.empty(shape=(0, 2))
+        dc.persistence_[1] == np.empty(shape=(0, 2)).astype(np.float64)
     ).all()
